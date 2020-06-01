afce0445b2f544a05f9a42afcea035e4e6484ae4
vitoriareges <vitoriareges@hotmail.com>
2020-06-01 16:36:57 +0000

    Login page

--- final-project/src/app/pages/tabs/tabs-routing.module.ts
+++ final-project/src/app/pages/tabs/tabs-routing.module.ts
@@ -5,10 +5,36 @@ import { TabsPage } from './tabs.page';
 
 const routes: Routes = [
   {
+    path: 'tabs',
+    component: TabsPage,
+   children: [
+      {
+        path: 'home',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('../home/home.module').then( m => m.HomePageModule)
+          }
+        ]  
+  },
+{
+        path: 'login',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('../login/login.module').then( m => m.LoginPageModule)
+          }
+        ]
+      }
+    ]
+},
+  
+   {
     path: '',
-    component: TabsPage
+    redirectTo: '/tabs/home',
+    pathMatch: 'full'
   }
-];
+]
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
